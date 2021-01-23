5cec2d1f2f297a9994dbdd3941ebea57dc7f07ec
lukevangorder <lukevangorder@gmail.com>
2021-01-23 14:01:02 -0500

    commit tree 24681a3e66ea432afdfeacfad29cdaa47ca8c1ac

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/)
+    array.collect do |sentence|
+      if sentence.length == 0
+        array.delete_at(array.index(sentence))
+      end
+    end
+    array.count
   end
 end
